a8ba4304486612aaeea8b09ff890383fc94f3dc3
Posh-in-Tech <155797065+Posh-in-Tech@users.noreply.github.com>
2024-01-15 17:32:40 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -86,7 +86,6 @@ function displayForecast(response) {
 }
    let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
-}
     
 let searchFormElemnt = document.querySelector("#search-form");
 searchFormElemnt.addEventListener("submit", handleSearchSubmit);
