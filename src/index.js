fbd9f4e9625ecab117dc51b10ce0fac70cf966f5
Posh-in-Tech <155797065+Posh-in-Tech@users.noreply.github.com>
2024-01-15 15:55:34 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ function formatDay(timestamp){
   let date= new Date(timestamp *1000)
   let days=["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   return days[date.getDay()];
-
+}
 function getForecast(city){
  let apiKey="3fbddf9ed4t6a6835a713f4c067a6o68";
 let apiUrl=`https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
