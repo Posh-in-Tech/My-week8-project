da67bb05124980dd59be5f0f575ec659ff49cf2e
Posh-in-Tech <155797065+Posh-in-Tech@users.noreply.github.com>
2024-01-15 14:21:23 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ searchCity("Durban");
 function getForecast(city){
   apiKey="3fbddf9ed4t6a6835a713f4c067a6o68";
   apiUrl=`https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
-axiois.get(apiUrl).then(displayForecast);
+axios.get(apiUrl).then(displayForecast);
 }
 
 function displayForecast(response) {
